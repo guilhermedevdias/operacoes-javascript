e264ccd377374eddd228cf85c0f6e047df07c3f9
guilhermedevdias <guilhermedias.desenvolvedor@gmail.com>
2022-06-18 08:53:43 -0300

    Operações concluidas

---
+++ js/script.js
@@ -0,0 +1,292 @@
+function obtersoma() {
+    var a = 10;
+    var b = 15;
+
+    var soma = a + b;
+    document.getElementById("resultado").innerHTML = "Resultado:" + soma;
+}
+
+function obtersomas() {
+    var c = "10";
+    var d = 2;
+
+    var somai = c + d;
+    document.getElementById("resultado1").innerHTML = "Resultado:" + somai;
+}
+
+function obtermult() {
+    var e = "10";
+    var f = 2;
+
+    var mul = e * f;
+    document.getElementById("resultado2").innerHTML = "Resultado:" + mul;
+}
+
+function obterdivi() {
+    var g = "10";
+    var h = 3;
+
+    var divis = g / h;
+    document.getElementById("resultado3").innerHTML = "Resultado:" + divis;
+}
+
+function obterporcent() {
+    var i = "10";
+    var j = 3;
+
+    var porcen = i % j;
+    document.getElementById("resultado4").innerHTML = "Resultado:" + porcen;
+}
+
+function obterboo() {
+    var k = 10;
+    var l = true;
+
+    var boo = k + l;
+    document.getElementById("resultado5").innerHTML = "Resultado:" + boo;
+}
+
+function obterigual() {
+    var m = 10;
+    var n = "10";
+
+    var igua = m == n;
+    document.getElementById("resultado6").innerHTML = "Resultado:" + igua;
+}
+
+function obterdiferente() {
+    var o = 10;
+    var p = "10";
+
+    var difere = o === p;
+    document.getElementById("resultado7").innerHTML = "Resultado:" + difere;
+}
+
+function obtermenor() {
+    var q = 10;
+    var r = 11;
+
+    var meno = q < r;
+    document.getElementById("resultado8").innerHTML = "Resultado:" + meno;
+}
+
+function obtermaior() {
+    var s = 10;
+    var t = 12;
+
+    var maio = s > t;
+    document.getElementById("resultado9").innerHTML = "Resultado:" + maio;
+}
+
+function obtermenorigual() {
+    var u = 10;
+    var v = 10.1;
+
+    var menorigua = u <= v;
+    document.getElementById("resultado10").innerHTML = "Resultado:" + menorigua;
+}
+
+function obtermaiorr() {
+    var y = 10;
+    var w = 9.99;
+
+    var maiorrr = y > w;
+    document.getElementById("resultado11").innerHTML = "Resultado:" + maiorrr;
+}
+
+function obterdiferesentenca() {
+    var aa = 10;
+    var bb = "dez";
+
+    var diferesente = aa != bb;
+    document.getElementById("resultado12").innerHTML = "Resultado:" + diferesente;
+}
+
+function obtertr() {
+    var cc = 10;
+    var dd = true;
+
+    var tru = cc + dd;
+    document.getElementById("resultado13").innerHTML = "Resultado:" + tru;
+}
+
+function obterbool() {
+    var ee = "dez";
+    var ff = true;
+
+    var boole = ee + ff;
+    document.getElementById("resultado14").innerHTML = "Resultado:" + boole;
+}
+
+function obterfalse() {
+    var gg = 10;
+    var hh = false;
+
+    var fal = gg + hh;
+    document.getElementById("resultado15").innerHTML = "Resultado:" + fal;
+}
+
+function obtermultiplicar() {
+    var ii = 10;
+    var jj = false;
+
+    var falss = ii * jj;
+    document.getElementById("resultado16").innerHTML = "Resultado:" + falss;
+}
+
+function obtertrue() {
+    var kk = true;
+    var ll = true;
+
+    var truee = kk * ll;
+    document.getElementById("resultado17").innerHTML = "Resultado:" + truee;
+}
+
+function obtersinais() {
+    var mm = 10;
+
+    var sina = mm--;
+    document.getElementById("resultado18").innerHTML = "Resultado:" + sina;
+}
+
+function obteriguais() {
+    var nn = 10;
+
+    var sinas = nn++;
+    document.getElementById("resultado19").innerHTML = "Resultado:" + sinas;
+}
+
+function obtercomer() {
+    var oo = 1;
+    var pp = 1;
+
+    var comerci = oo & pp;
+    document.getElementById("resultado20").innerHTML = "Resultado:" + comerci;
+}
+
+function obtercomerc() {
+    var qq = 1;
+    var rr = 0;
+
+    var comercia = qq & rr;
+    document.getElementById("resultado21").innerHTML = "Resultado:" + comercia;
+}
+
+function obtercomercial() {
+    var ss = 0;
+    var tt = 0;
+
+    var comerciall = ss & tt;
+    document.getElementById("resultado22").innerHTML = "Resultado:" + comerciall;
+}
+
+function obtercomercialll() {
+    var uu = 1;
+    var vv = 0;
+
+    var comercialls = uu & vv;
+    document.getElementById("resultado23").innerHTML = "Resultado:" + comercialls;
+}
+
+function obterdivisoe() {
+    var ww = 0;
+    var yy = 1;
+
+    var diviso = ww & yy;
+    document.getElementById("resultado24").innerHTML = "Resultado:" + diviso;
+}
+
+function obterex() {
+    var aaa = 5;
+    var bbb = 5;
+    var ccc = 10;
+
+    var exp = aaa + bbb == ccc;
+    document.getElementById("resultado25").innerHTML = "Resultado:" + exp;
+}
+
+function obterexp() {
+    var ddd = "5";
+    var eee = "5";
+    var fff = 10;
+
+    var expr = ddd + eee == fff;
+    document.getElementById("resultado26").innerHTML = "Resultado:" + expr;
+}
+
+function obterexpr() {
+    var ggg = "5";
+    var hhh = 2;
+    var iii = 9;
+
+    var expre = ggg * hhh > iii;
+    document.getElementById("resultado27").innerHTML = "Resultado:" + expre;
+}
+
+function obterexpre() {
+    var jjj = 10;
+    var kkk = 10;
+    var lll = 2;
+
+    var expres = (jjj + kkk) * lll;
+    document.getElementById("resultado28").innerHTML = "Resultado:" + expres;
+}
+
+function obterexpressao() {
+    var mmm = 10;
+    var nnn = 10;
+    var ooo = 2;
+
+    var express = mmm + nnn * ooo;
+    document.getElementById("resultado29").innerHTML = "Resultado:" + express;
+}
+
+function obterbranco() {
+    var branco = "preto";
+
+    var branc = branco == branco;
+    document.getElementById("resultado01").innerHTML = "Resultado:" + branc;
+}
+
+function obterbranco1() {
+    var branco = "preto";
+
+    var branc = branco == branco;
+    document.getElementById("resultado01").innerHTML = "Resultado:" + branc;
+}
+
+function obtercinza1() {
+    var branco = "preto";
+    var cinza = "branco";
+
+    var brancoo = branco == cinza;
+    document.getElementById("resultado02").innerHTML = "Resultado:" + brancoo;
+}
+
+function obterbranco3() {
+    var carro1 = "preto";
+    var branco2 = "preto";
+
+    var brancoo1 = carro1 === branco2;
+    document.getElementById("resultado03").innerHTML = "Resultado:" + brancoo1;
+}
+
+function obteroperacoes () {
+    var valor = 30000;
+    var entrada = 3000;
+    var resultado = 27000
+    var prestacao = 750;
+
+    var resulta = valor - entrada * (resultado / prestacao);
+    document.getElementById("resultado04").innerHTML = "Resultado" + resulta;
+}
+
+function caracteres() {
+    let branco = "preto"
+    let preto = "cinza"
+    let cinza = "branco"
+
+    let resultad = branco.length+preto.length+cinza.length;
+    document.getElementById("resultado05").innerHTML = "Resultado:" + resultad;
+}
+
